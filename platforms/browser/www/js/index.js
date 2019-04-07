f36246e8a3ec2f5ce4067e11007a03cdbcca76c3
Gaute Meek Olsen <gaute.meek@gmail.com>
2019-04-07 16:35:43 +0200

    rearranged directory

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,53 @@
+const app = {
+    playButton: null,
+    isPlaying: false,
+    audio: new Audio('http://mms-live.online.no/oradio_mp3_m'),
+    isToggling: false,
+    currentDisplayed: '',
+
+    // Application Constructor
+    initialize: function () {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function () {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function (id) {
+        this.playButton = document.querySelector('.play-button');
+        this.playButton.addEventListener('click', () => {
+            if (!this.isToggling) {
+                this.isPlaying = !this.isPlaying;
+                if (this.isPlaying) {
+                    this.playRadio();
+                } else {
+                    this.stopRadio();
+                }
+                this.isToggling = true;
+                setTimeout(() => this.isToggling = false, 500);
+            }
+        });
+    },
+
+    stopRadio: function () {
+        this.audio.pause();
+        document.querySelector('.stop-symbol').classList.add('hidden');
+        setTimeout(() => document.querySelector('.play-symbol').classList.remove('hidden'), 500);
+        document.querySelector('.waveHorizontals').classList.remove('playing');
+    },
+
+    playRadio: function () {
+        this.audio.play();
+        document.querySelector('.play-symbol').classList.add('hidden');
+        setTimeout(() => document.querySelector('.stop-symbol').classList.remove('hidden'), 500);
+        document.querySelector('.waveHorizontals').classList.add('playing');
+    }
+};
+
+app.initialize();
